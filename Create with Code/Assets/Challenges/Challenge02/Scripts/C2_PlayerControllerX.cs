6e2ebd323f0448a7c2572728e57159aa09e81e7e
Varun Anirudh Tirunagari <varunanirudh1811@gmail.com>
2025-12-29 09:08:52 +0530

    Add Challenge 2 Unity scene and related assets
    Introduced the Challenge 2 folder with a new Unity scene, prefabs, scripts, materials, lighting data. This adds all necessary files for the Challenge 2 gameplay and development.

---
+++ Create with Code/Assets/Challenges/Challenge02/Scripts/C2_PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C2_PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 1.0f;
+    private float timer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= delay )
+        {           
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0.0f;
+        }
+    }
+}
